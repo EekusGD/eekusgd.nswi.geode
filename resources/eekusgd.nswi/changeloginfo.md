145cc5ed152a64fb85640b29d3a29bdc36b037be
EekusGD <andrewche321@gmail.com>
2024-09-28 22:08:33 +0300

    Add files via upload

---
+++ resources/eekusgd.nswi/changeloginfo.md
@@ -0,0 +1,7 @@
+#v1.0.1
+
+* bugfixes
+
+#v1.0.0
+
+* First release
---
+++ resources/eekusgd.nswi/saveaccount.js
@@ -0,0 +1,19 @@
+this.saveaccount = function() {
+
+this.addmore = "" + Math.floor(Math.random() * 99999) + 1
+
+var username = addmore + {
+stars: "currentStars",
+moons: "currentMoons",
+diamonds: "currentDiamonds"
+}
+}
+
+const save = function() {
+this.currentPlayer = username
+if (currentPlayer < username) {
+username.repeat(1)
+}
+
+saveaccount();
+save();
